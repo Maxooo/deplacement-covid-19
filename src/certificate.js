b0d2f5b0e964b6c54ad49c56f800d243e0fcaa1b
Maxime Corson <mcorson@wynd.eu>
2020-04-22 18:24:15 +0200

    Improve profiles history display

--- src/certificate.js
+++ src/certificate.js
@@ -106,18 +106,41 @@ function setProfilesHistory (profiles) {
 }
 
 function displayHistoryProfiles () {
-  const table = $('#profile-history table tbody')
+  const table = $('#profile-history .profiles')
+  const info = $('#profile-history .message')
+
   let html = ''
   table.innerHTML = html
   const profiles = getHistoryProfiles()
-  for (let i = 0; i < profiles.length; i++) {
-    const profile = profiles[i]
+  if (profiles.length === 0) {
+    info.innerHTML = '<p>Vous n\'avez généré aucune attestation depuis cet appareil ou ce navigateur.</p>' +
+    '<p>Générez une première attestation pour pouvoir la réutiliser ici lors de vos prochains déplacements.</p>'
+  } else {
+    for (let i = 0; i < profiles.length; i++) {
+      const profile = profiles[i]
+      info.innerHTML = '<p>L\'attestation générée portera comme information de sortie, l\'heure et la date actuelles.</p>'
 
-    html += `<tr><td>${profile.firstname} ${profile.lastname}</td>` +
-    `<td><button class="generate-attestation btn btn-sm btn-primary" data-id="${i}">Attestation</button></td>` +
-    `<td><button class="remove-profile btn btn-sm btn-danger" data-id="${i}">X</button></td></tr>`
+      const reasons = profile.reasons.split('-')
+      let reasonsHtml = ''
+      for (let j = 0; j < reasons.length; j++) {
+        reasonsHtml += `<span class="badge badge-danger mr-1">${reasons[j]}</span>`
+      }
+
+      html += `<li class="list-group-item">
+      <button class="float-right btn text-muted card-link remove-profile" data-id="${i}"><i class="fa fa-times"></i></button>
+      <h5 class="card-title mb-0">${profile.firstname} ${profile.lastname}</h5>
+      <small class="card-subtitle mb-2 text-muted">Né(e) le ${profile.birthday} à ${profile.lieunaissance}</small>
+      <p class="card-text">
+      ${profile.address}<br />
+      ${profile.zipcode}, ${profile.town}
+      </p>
+      <p class="float-left">${reasonsHtml}</p>
+      <button class="float-right generate-attestation btn btn-sm btn-primary" data-id="${i}"><i class="fa fa-file-pdf"></i> Générer l'attestation</button>
+      <div class="clearfix"></div>
+    </li>`
+      table.innerHTML = html
+    }
   }
-  table.innerHTML = html
 }
 
 function idealFontSize (font, text, maxWidth, minSize, defaultSize) {
